6440fcc6db39a9d9b9f869d6d7d71e7a70e5fe42
Coda Hale <coda.hale@gmail.com>
2016-09-29 13:22:38 -0700

    Disable timestamps on log statements.

--- hello.go
+++ hello.go
@@ -7,6 +7,11 @@ import (
 	"os"
 )
 
+func init() {
+	// disable timestamps in the log format -- that's handled by syslog
+	log.SetFlags(0)
+}
+
 func Log(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
