881145bac6255fdb630f014109badb9203e22f5b
Coda Hale <coda.hale@gmail.com>
2016-09-29 13:24:09 -0700

    Rename log middleware.

--- hello.go
+++ hello.go
@@ -12,7 +12,7 @@ func init() {
 	log.SetFlags(0)
 }
 
-func Log(handler http.Handler) http.Handler {
+func withLogs(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
 		handler.ServeHTTP(w, r)
@@ -31,7 +31,7 @@ func main() {
 	http.Handle("/", http.FileServer(http.Dir("assets")))
 	addr := getAddr()
 	log.Printf("Listening on %s", addr)
-	if err := http.ListenAndServe(addr, Log(http.DefaultServeMux)); err != nil {
+	if err := http.ListenAndServe(addr, withLogs(http.DefaultServeMux)); err != nil {
 		log.Fatal(err)
 	}
 }
